08685e7c0fd8a9d60a6201f6764c9ff5d9be5314
kdongk <ldh5081@naver.com>
2023-05-30 00:33:18 +0900

    Initial commit

---
+++ src/main/java/com/onehana/server_ilogu/ServerILogUApplication.java
@@ -0,0 +1,13 @@
+package com.onehana.server_ilogu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerILogUApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerILogUApplication.class, args);
+    }
+
+}
