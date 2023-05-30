97c38dd9cdbe95fe571c4517349cd28bb83eaebb
kdongk <ldh5081@naver.com>
2023-05-30 21:58:43 +0900

    add: SwaggerConfig 설정 추가

---
+++ src/main/java/com/onehana/server_ilogu/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.onehana.server_ilogu.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "ILogU API 명세서",
+                description = "ILogU",
+                version = "v1")
+)
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi user() {
+        String[] paths = {"/user/**"};
+
+        return GroupedOpenApi.builder()
+                .group("유저")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
