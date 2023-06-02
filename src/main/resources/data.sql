4aa173d9ad9260071011c9b33f19250440e5cb51
kdongk <ldh5081@naver.com>
2023-06-02 19:50:48 +0900

    add: Security 설정에 filter 제외 코드 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into ILogU.user(created_at, updated_at, email, nickname, password, user_role)
-VALUES (NOW(), NOW(), '1@naver.com', 'kdk', '1234', 'USER_ROLE');
-
-insert into ILogU.user(created_at, updated_at, email, nickname, password, user_role)
-VALUES (NOW(), NOW(), '2@naver.com', '김광렬', '1234', 'USER_ROLE');
+# insert into ILogU.user(created_at, updated_at, email, nickname, password, user_role)
+# VALUES (NOW(), NOW(), '1@naver.com', 'kdk', '1234', 'USER_ROLE');
+#
+# insert into ILogU.user(created_at, updated_at, email, nickname, password, user_role)
+# VALUES (NOW(), NOW(), '2@naver.com', '김광렬', '1234', 'USER_ROLE');
 
