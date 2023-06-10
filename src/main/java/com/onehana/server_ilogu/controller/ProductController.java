1927b3bdada8524e9a8935546594c3e2bfe249a5
kdongk <ldh5081@naver.com>
2023-06-10 23:28:43 +0900

    add: product controller 스웨거 추가

--- src/main/java/com/onehana/server_ilogu/controller/ProductController.java
+++ src/main/java/com/onehana/server_ilogu/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.onehana.server_ilogu.dto.response.BaseResponse;
 import com.onehana.server_ilogu.entity.Product;
 import com.onehana.server_ilogu.entity.enums.ProductType;
 import com.onehana.server_ilogu.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,12 +21,14 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @Operation(summary = "금융 상품 전체 조회", description = "금융 상품을 전체 조회한다.")
     @GetMapping
     public BaseResponse<List<ProductDto>> getProducts() {
         List<ProductDto> products = productService.getProducts();
         return new BaseResponse<>(products);
     }
 
+    @Operation(summary = "금융 상품 종류 별 조회", description = "예금-적금 등 종류에 따라 상품을 조회한다.")
     @GetMapping("/type/{type}")
     public BaseResponse<List<ProductDto>> getProductsByType(@PathVariable ProductType type) {
         List<ProductDto> products = productService.getProductsByType(type);
--- src/main/java/com/onehana/server_ilogu/entity/Product.java
+++ src/main/java/com/onehana/server_ilogu/entity/Product.java
@@ -8,15 +8,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Product {
+public class Product extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
