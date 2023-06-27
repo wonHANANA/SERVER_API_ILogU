b6891b6045b9e08fb08c00021cb5ad54caa6a287
kdongk <ldh5081@naver.com>
2023-06-27 10:38:56 +0900

    fix: 아이에게 송금하기 response 수정

--- src/main/java/com/onehana/server_ilogu/controller/FamilyController.java
+++ src/main/java/com/onehana/server_ilogu/controller/FamilyController.java
@@ -42,7 +42,7 @@ public class FamilyController {
 
     @Operation(summary = "피드글을 통해 아이에게 송금하기", description = "우리 아이에게 돈을 송금한다.")
     @PostMapping("/board/{boardId}/balance/{balance}")
-    public BaseResponse<List<UserDto>> sendMoneyToChild(@AuthenticationPrincipal CustomUserDetails userDetails,
+    public BaseResponse<Void> sendMoneyToChild(@AuthenticationPrincipal CustomUserDetails userDetails,
                                                         @PathVariable Long boardId, @PathVariable BigDecimal balance) {
         familyService.sendMoneyToChild(userDetails.getEmail(), boardId, balance);
         return new BaseResponse<>(SUCCESS);
