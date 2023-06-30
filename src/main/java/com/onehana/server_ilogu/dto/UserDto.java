c53c2ecade09c6216a28c7de5bc70824e5c4a590
kdongk <ldh5081@naver.com>
2023-06-30 16:44:17 +0900

    fix: userDto 내용 수정

--- src/main/java/com/onehana/server_ilogu/dto/UserDto.java
+++ src/main/java/com/onehana/server_ilogu/dto/UserDto.java
@@ -1,8 +1,6 @@
 package com.onehana.server_ilogu.dto;
 
-import com.onehana.server_ilogu.dto.request.UserJoinRequest;
 import com.onehana.server_ilogu.entity.User;
-import com.onehana.server_ilogu.entity.enums.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,34 +8,12 @@ import lombok.Data;
 @AllArgsConstructor
 public class UserDto {
     private String email;
-    private String password;
-    private String simplePassword;
     private String nickname;
-    private String username;
-    private String phone;
-    private UserRole userRole;
-
-    public static UserDto of(UserJoinRequest request) {
-        return new UserDto(
-                request.getEmail(),
-                request.getPassword(),
-                request.getSimplePassword(),
-                request.getNickname(),
-                request.getUsername(),
-                request.getPhone(),
-                UserRole.USER_ROLE
-        );
-    }
 
     public static UserDto of(User user) {
         return new UserDto(
                 user.getEmail(),
-                user.getPassword(),
-                user.getSimplePassword(),
-                user.getNickname(),
-                user.getUsername(),
-                user.getPhone(),
-                user.getUserRole()
+                user.getNickname()
         );
     }
 }
